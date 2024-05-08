9a725edd5234993424920d270038c1eb987fbca9
mschute <46330682+mschute@users.noreply.github.com>
2024-05-08 20:39:44 +0100

    Updated UI

--- PageTurnerBooks/Views/SignUpView.swift
+++ PageTurnerBooks/Views/SignUpView.swift
@@ -7,28 +7,44 @@ struct SignUpView: View {
     @State private var fullName = ""
     @State private var password = ""
     @State private var confirmPassword = ""
+    @FocusState private var fieldIsFocused: Bool
 
     @EnvironmentObject var authViewModel: AuthViewModel
     var body: some View {
+        //TODO: Style choice ignore safe edges? or visit sign in page for other view
             VStack {
                 Text("Register")
                     .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.pTPrimary)
+                
                 Spacer()
                 VStack(spacing: 25) {
                     // Email input field
                     InputField(text: $email, title: "Email Address", placeholder: "name@example.com", isSecureField: false)
+                        .keyboardType(.emailAddress)
+                        .focused($fieldIsFocused)
                         .autocapitalization(.none)
 
                     // Full name input field
                     InputField(text: $fullName, title: "Full Name", placeholder: "John Doe", isSecureField: false)
+                        .keyboardType(.default)
+                        .focused($fieldIsFocused)
 
                     // Password input field
                     InputField(text: $password, title: "Password", placeholder: "Enter your password", isSecureField: true)
                         .autocapitalization(.none)
+                        .keyboardType(.default)
+                        .focused($fieldIsFocused)
 
                     // Confirm Password input field
                     InputField(text: $confirmPassword, title: "Confirm Password", placeholder: "Confirm your password", isSecureField: true)
                         .autocapitalization(.none)
+                        .keyboardType(.default)
+                        .focused($fieldIsFocused)
                 }
                 .padding(.horizontal)
                 .padding(.top, 10)
@@ -36,7 +52,6 @@ struct SignUpView: View {
                 // Register button can be added here.
                 NavigationLink(destination: SignInView()) {
                                     Text("Already have an account? Sign In")
-                        
                                         .foregroundColor(.blue)
                                 }
                 .padding(.top, 30)
@@ -60,7 +75,6 @@ struct SignUpView: View {
                 .navigationBarBackButtonHidden(true)
                 Spacer()
             }
-            .padding()
     }
 }
 
