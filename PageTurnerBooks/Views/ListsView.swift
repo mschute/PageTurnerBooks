f8422a31309f8483a27a1573545a7e580ba60c3b
bxb1pro <bxb1pro@gmail.com>
2024-05-10 19:10:17 +0100

    ListsView UI
    Added picture links for all three lists, lined header up with other pages.

--- PageTurnerBooks/Views/ListsView.swift
+++ PageTurnerBooks/Views/ListsView.swift
@@ -7,39 +7,67 @@ struct ListsView: View {
     @ObservedObject var viewModel: BooksListViewModel
     
     var body: some View {
-        
         NavigationStack {
             ZStack {
-                Color.pTPrimary.edgesIgnoringSafeArea(.top)
-                
+//                Color.pTPrimary.edgesIgnoringSafeArea(.top)
                 
                 VStack(spacing: 0){
-                        Text("Reading List")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity, maxHeight: 0)
-                            .background(Color.pTPrimary)
-                            .padding(.top, 80)
-                            .padding(.bottom, -20)
-                    .background(Color.pTPrimary)
-                    .edgesIgnoringSafeArea(.top)
-                    List {
-                        Section {
+                    Text("Reading List")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, maxHeight: 0)
+                        .background(Color.pTPrimary)
+                        .padding(.top, 83)
+                        .padding(.bottom, 40)
+                        .background(Color.pTPrimary)
+                        .edgesIgnoringSafeArea(.top)
+                    
+                    ScrollView {
+                        VStack {
+                            // Picture link for "Want To Read"
                             NavigationLink(destination: ListWantToReadView(viewModel: viewModel)) {
-                                Text("Want to Read")
+                                Image("WantToRead") // Set this image in your assets
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 350, height: 150)
+                                    .overlay(Rectangle().foregroundColor(Color.black.opacity(0.4)))
+                                    .overlay(Text("Want to Read")
+                                        .font(.system(size: 24, weight: .bold))
+                                        .foregroundColor(.white))
+                                    .cornerRadius(10)
                             }
-                            
+                            .padding(.bottom, 20)
+
+                            // Picture link for "Currently Reading"
                             NavigationLink(destination: ListCurrentlyReadingView(viewModel: viewModel)) {
-                                Text("Currently Reading")
+                                Image("CurrentlyReading") // Set this image in your assets
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 350, height: 150)
+                                    .overlay(Rectangle().foregroundColor(Color.black.opacity(0.4)))
+                                    .overlay(Text("Currently Reading")
+                                        .font(.system(size: 24, weight: .bold))
+                                        .foregroundColor(.white))
+                                    .cornerRadius(10)
                             }
-                            
+                            .padding(.bottom, 20)
+
+                            // Picture link for "Finished Reading"
                             NavigationLink(destination: ListFinishedReadingView(viewModel: viewModel)) {
-                                Text("Finished Reading")
+                                Image("FinishedReading") // Set this image in your assets
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 350, height: 150)
+                                    .overlay(Rectangle().foregroundColor(Color.black.opacity(0.37)))
+                                    .overlay(Text("Finished Reading")
+                                        .font(.system(size: 24, weight: .bold))
+                                        .foregroundColor(.white))
+                                    .cornerRadius(10)
                             }
                         }
                     }
-                    .listStyle(GroupedListStyle())
+                    .frame(maxWidth: .infinity)
                 }
                 .navigationBarTitle("", displayMode: .inline)
             }
@@ -52,4 +80,3 @@ struct ListsView_Preview: PreviewProvider {
         ListsView(viewModel: BooksListViewModel(userId: "9laC5umqf4T6fviudjD6HcuN1pW2"))
     }
 }
-
